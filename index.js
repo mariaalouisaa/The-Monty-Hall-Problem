558e85f3b0442e66d94e4e698c7f1fa0e18ebbc4
= <maria.louisa.castro@gmail.com>
2022-03-28 20:08:41 +0100

    Add function to reset and begin table update

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// generate a number from 1 - 10
-// user clicks on a door
-// the number is reveled
-// result displayed in table
-// guesses updates
 const doors = Array.from(document.querySelectorAll(".doors p"));
 
 const generateNum = () => {
@@ -10,14 +5,20 @@ const generateNum = () => {
 };
 
 let number = generateNum();
+let guesses = 8;
+let result = 0;
 
 const userSelectsDoor = (e) => {
   if (Number(e.target.id) === number) {
     e.target.style.background = "green";
+    result++;
   } else {
     e.target.style.background = "red";
     doors[number].style.background = "yellow";
   }
+  guesses--;
+  document.querySelector("#guessed").innerHTML = guesses;
+  if (guesses === 0) eightGuessed();
   resetCards();
 };
 
@@ -26,6 +27,13 @@ const resetCards = () => {
     doors.forEach((door) => (door.style.background = "none"));
     number = generateNum();
   }, 1000);
+};
+
+const eightGuessed = () => {
+  document.querySelector(".message").innerHTML = "NEW ATTEMPT!";
+  //update table
+  guesses = 8;
+  document.querySelector("#guessed").innerHTML = guesses;
 };
 
 doors.forEach((door) => door.addEventListener("click", userSelectsDoor));
