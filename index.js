7186622b0f474bc765875c570f1c1072379e0662
= <maria.louisa.castro@gmail.com>
2022-03-28 19:49:12 +0100

    Add functionality for correct guess to be established with colours for the user to see

--- index.js
+++ index.js
@@ -3,15 +3,29 @@
 // the number is reveled
 // result displayed in table
 // guesses updates
+const doors = Array.from(document.querySelectorAll(".doors p"));
 
 const generateNum = () => {
   return Math.floor(Math.random() * 7);
 };
 
-const userSelectsDoor = () => {
-  console.log("clicked");
+let number = generateNum();
+
+const userSelectsDoor = (e) => {
+  if (Number(e.target.id) === number) {
+    e.target.style.background = "green";
+  } else {
+    e.target.style.background = "red";
+    doors[number].style.background = "yellow";
+  }
+  resetCards();
 };
 
-Array.from(document.querySelectorAll(".doors p")).forEach((door) =>
-  door.addEventListener("click", userSelectsDoor)
-);
+const resetCards = () => {
+  setTimeout(() => {
+    doors.forEach((door) => (door.style.background = "none"));
+    number = generateNum();
+  }, 1000);
+};
+
+doors.forEach((door) => door.addEventListener("click", userSelectsDoor));
